8761b470b8fd5ecab6655c62ccc7e755b768d1ed
Dylan Cunningham <gdylanc@gmail.com>
2021-12-15 10:53:09 -0700

    hooks up bootstrap & bootstrap-icons

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@
 function App() {
   return (
     <div className="App">
-test
+      <h1 className="text-primary">
+        <i className="bi bi-diagram-2-fill"></i> Repos
+      </h1>
     </div>
   );
 }
